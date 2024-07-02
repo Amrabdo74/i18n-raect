995b80680132d79cb8c59a659df3d311183214eb
Amrabdo74 <andkpe12@gmail.com>
2024-07-02 20:22:34 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './i18n'; // import the i18n configuration
+import BackToTopButton from './Top.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <BackToTopButton/>
   </React.StrictMode>,
 )
